630c2bcad5e7a58ccea6574083433f4d7fb1f180
krlslman <selmankorall@gmail.com>
2022-05-13 23:18:59 +0300

    minor changes

--- app.js
+++ app.js
@@ -9,3 +9,12 @@ function activeLink(){
 li.forEach((item) =>
 item.addEventListener('click',activeLink));
 
+function goToPage5() {
+	document.getElementById('PageText').innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus ea magni sed debitis dignissimos, aperiam nemo minus soluta repudiandae porro alias perspiciatis asperiores, commodi dicta doloremque molestiae eos quas rerum.";
+
+	document.getElementById("PageImage").src = "https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80";
+}
+
+
+document.getElementsByClassName("iiiii").onclick=getElementsByClassName("content").innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus ea magni sed debitis dignissimos, aperiam nemo minus soluta repudiandae porro alias perspiciatis asperiores, commodi dicta doloremque molestiae eos quas rerum.";
+// 'changeImage("1772031_29_b.jpg");
